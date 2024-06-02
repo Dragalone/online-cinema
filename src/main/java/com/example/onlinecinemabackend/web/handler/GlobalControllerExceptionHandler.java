5e2f08ef21f8ee76f796f674dc7ad68ee8615ce7
Vladimir <vova.kaverya@mail.ru>
2024-06-02 15:30:49 +0300

    exception handler

---
+++ src/main/java/com/example/onlinecinemabackend/web/handler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.example.onlinecinemabackend.web.handler;
+
+import com.example.onlinecinemabackend.exception.AccessDeniedException;
+import com.example.onlinecinemabackend.exception.AlreadyExistsException;
+import com.example.onlinecinemabackend.exception.EntityNotFoundException;
+import com.example.onlinecinemabackend.web.dto.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFound(EntityNotFoundException ex) {
+        log.error("Error trying to get entity", ex);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> accessDenied(AccessDeniedException ex) {
+        log.error("Error trying to change entity", ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> alreadyExists(AlreadyExistsException ex) {
+        log.error("Error trying to create or update entity", ex);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> notValid(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> errorMessages = bindingResult.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        String errorMessage = String.join("; ", errorMessages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+}
